6ac89031c2dbb20b5fa039df94ca3e9bc9967003
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-10-30 04:25:27 +0500

    2-misol

--- bin/string_length02.dart
+++ bin/string_length02.dart
@@ -6,6 +6,20 @@
     Returns:
         True or False
      */
+import 'package:test/test.dart';
+
+String func(String a) {
+  String b = "text";
+  String c = 'True';
+  String h = 'False';
+  if (a.length == b.length) {
+    return c;
+  } else {
+    return h;
+  }
+}
+
 void main() {
+  print(func('okta'));
   // write your code here
 }
