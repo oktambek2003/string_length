6ae06abce8aa662a6eb0f1eb6ad7c50770f299b7
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-10-30 15:34:38 +0500

    8-misol

--- bin/string_length08.dart
+++ bin/string_length08.dart
@@ -7,6 +7,20 @@
     Returns:
         str: answer
 */
+String func(String s) {
+  int b = s.length;
+  int c = b % 2;
+  if (c == 1) {
+    return(s[b ~/ 2]);
+  } else {
+    String d = (s[(b ~/ 2)-1]);
+    String f = (s[(b ~/ 2)]);
+    return(d + f);
+  }
+
+}
+
 void main() {
+  print(func("o'ktam"));
   // write your code here
 }
