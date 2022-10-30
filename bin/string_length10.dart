080557be15ab5f318a62f34c6ff640fe5dbc37e7
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-10-30 17:32:52 +0500

    10-misol

--- bin/string_length10.dart
+++ bin/string_length10.dart
@@ -6,7 +6,12 @@
     Returns:
         bool: answer
 */
+String func(String a) {
+  String b = a.toUpperCase();
+  return b;
+}
 
 void main() {
+  print(func("oktam"));
   // write your code here
 }
