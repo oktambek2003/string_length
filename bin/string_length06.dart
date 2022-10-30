7b50245557a89ce326f600a6f35fec7ad45288db
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-10-30 14:44:08 +0500

    6-misol

--- bin/string_length06.dart
+++ bin/string_length06.dart
@@ -1,5 +1,4 @@
 /*
-    Create function func with function arguments 's1' and 's2'
     Given two strings, s1 and s2. Return the shortest length between them.
     Args:
         s1: string
@@ -7,18 +6,15 @@
     Returns:
         shortest string
 */
-int func(String s1, String s2) {
+String func(String s1, String s2) {
   int a = s1.length;
   int b = s2.length;
-  int c = a - b;
-  if (a - b > 0) {
-    return c;
-  } else if (a - b < 0) {
-    return c *(-1);
-  } else {
-    return 0;
+  if (a > b) {
+    return s1;
   }
-  
+  {
+    return s2;
+  }
 }
 
 void main() {
